7c6d976692c7886ce113804e1452fcd93ba86715
Mykola Yuryshynets <kolan4ick@gmail.com>
2024-11-28 22:01:56 +0200

    [HOTFIX] Use next-client-cookies properly

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,12 +8,16 @@ import { loginUser } from "@/hooks/useUser";
 import { User } from "@/lib/types/User";
 import { getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
+import { CookiesProvider } from "next-client-cookies/server";
 import StoreProvider from "@/app/StoreProvider";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 import { ThemeProvider } from "@/app/providers/ThemeProvider";
 
-export default async function LocaleLayout({ children, params }: {
+export default async function LocaleLayout({
+                                               children,
+                                               params,
+                                           }: {
     children: React.ReactNode;
     params: Promise<{ locale: string }>;
 }) {
@@ -37,16 +41,17 @@ export default async function LocaleLayout({ children, params }: {
         <html lang={locale} className="h-full">
         <body className="flex flex-col min-h-screen text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-900">
         <ThemeProvider>
-            <NextIntlClientProvider messages={messages} locale={locale}>
-                <StoreProvider user={user} token={token}>
-                    <Navbar />
-                    {/* Main content wrapper */}
-                    <main className="flex-grow rounded-lg mx-4 md:mx-8 lg:mx-16 p-4 md:p-8">
-                        {children}
-                    </main>
-                    <Footer />
-                </StoreProvider>
-            </NextIntlClientProvider>
+            <CookiesProvider>
+                <NextIntlClientProvider messages={messages} locale={locale}>
+                    <StoreProvider user={user} token={token}>
+                        <Navbar />
+                        <main className="flex-grow rounded-lg mx-4 md:mx-8 lg:mx-16 p-4 md:p-8">
+                            {children}
+                        </main>
+                        <Footer />
+                    </StoreProvider>
+                </NextIntlClientProvider>
+            </CookiesProvider>
         </ThemeProvider>
         </body>
         </html>

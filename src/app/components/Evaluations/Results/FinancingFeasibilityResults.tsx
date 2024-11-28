f4854fccb6deb921d533af2048e558244c92344b
Mykola Yuryshynets <kolan4ick@gmail.com>
2024-11-28 03:27:31 +0200

    Adjust adaptiveness for financing feasibility results

--- src/app/components/Evaluations/Results/FinancingFeasibilityResults.tsx
+++ src/app/components/Evaluations/Results/FinancingFeasibilityResults.tsx
@@ -7,17 +7,16 @@ export default function FinancingFeasibilityResults({ financingFeasibility }: { 
     const { cone_shaped_membership, membership, triangle_membership } = financingFeasibility;
 
     return (
-        <div
-            className="p-6 bg-gradient-to-b from-blue-100 via-gray-200 to-gray-100 dark:from-gray-700 dark:via-gray-800 dark:to-gray-900 rounded-2xl shadow-lg border border-gray-300 dark:border-gray-600">
-            <h2 className="text-3xl font-bold text-center text-gray-800 dark:text-gray-100 mb-6">
+        <div className="p-4 md:p-6 bg-gradient-to-b from-blue-100 via-gray-200 to-gray-100 dark:from-gray-700 dark:via-gray-800 dark:to-gray-900 rounded-2xl shadow-lg border border-gray-300 dark:border-gray-600">
+            <h2 className="text-2xl md:text-3xl font-bold text-center text-gray-800 dark:text-gray-100 mb-4 md:mb-6">
                 {t("results.financing.title")}
             </h2>
 
-            <div className="mt-8">
-                <h3 className="text-xl font-bold text-gray-800 dark:text-gray-100 mb-4">
+            <div className="mt-6 md:mt-8">
+                <h3 className="text-lg md:text-xl font-bold text-gray-800 dark:text-gray-100 mb-4">
                     {t("results.financing.memberships")}
                 </h3>
-                <div className={`grid grid-cols-1 sm:grid-cols-2 gap-6`}>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <SummaryCard
                         title={t("results.financing.cone_shaped_membership")}
                         value={cone_shaped_membership.toFixed(4)}
@@ -34,8 +33,8 @@ export default function FinancingFeasibilityResults({ financingFeasibility }: { 
                     />
                 </div>
             </div>
-            <div className={"mt-8"}>
-                <h3 className="text-xl font-bold text-gray-800 dark:text-gray-100 mb-4">
+            <div className="mt-6 md:mt-8">
+                <h3 className="text-lg md:text-xl font-bold text-gray-800 dark:text-gray-100 mb-4">
                     {t("results.summary")}
                 </h3>
                 {triangle_membership.map((triangle: { value: number; linguistic: string; }, index: number) => (
@@ -46,7 +45,7 @@ export default function FinancingFeasibilityResults({ financingFeasibility }: { 
                                 {t("results.financing.triangle_membership")} - {t(`results.financing.triangle_memberships.${index + 1}`)}
                             </h3>
                         }
-                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
                             <SummaryCard
                                 title={`${t("results.financing.triangle_membership")}`}
                                 value={triangle.value.toFixed(4)}
